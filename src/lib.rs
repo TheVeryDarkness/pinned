5bfb5ccc9479803821e18a6a618ad65ce9dc0a7a
TheVeryDarkness <3266343194@qq.com>
2023-10-24 21:14:05 +0800

    Update documents and tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 //!
 //! A substitute for [Box::leak](https://doc.rust-lang.org/stable/alloc/boxed/struct.Box.html#method.leak).
 
+#![warn(missing_docs, rust_2021_compatibility, rust_2018_idioms)]
+
 extern crate alloc;
 
 use alloc::{boxed::Box, vec::Vec};
@@ -16,13 +18,28 @@ const PANIC: &'static str = "Another thread panicked while holding the lock.";
 
 /// A list of `Pin<Box<T>>`.
 ///
-/// I cannot implement this without a lock inside.
+/// One can keep the references to a lot of pinned items,
+/// whose lifetime is managed by the container,
+/// without holding a mutable reference to the container.
 ///
+/// ```rust
+/// use pinned_bucket::*;
+/// let v = PinnedList::new();
+/// let a = v.push(1);
+/// let b = v.push(2);
+/// assert_eq!(a, &1);
+/// assert_eq!(b, &2);
+/// ```
+/// 
+/// I cannot implement this without a lock inside.
 /// Borrow checker isn't so smart currently.
 ///
+/// As the items inside are still managed by the container,
+/// codes below won't compile.
+///
 /// ```compile_fail
-/// use pinned::PinnedVec;
-/// let v = PinnedVec::new();
+/// use pinned_bucket::*;
+/// let v = PinnedList::new();
 /// let a = v.push(1);
 /// drop(v);
 /// assert_eq!(a, &1);
@@ -95,5 +112,23 @@ mod tests {
         let b = v.push(2);
         assert_eq!(a, &1);
         assert_eq!(b, &2);
+    }
+
+    /// To ensure that allocated items won't be moved.
+    #[test]
+    fn resize() {
+        let v = PinnedList::with_capacity(4);
+        let cap = v.sections.read().expect(PANIC).capacity();
+        let refs: Vec<_> = (0..cap + 1)
+            .into_iter()
+            .map(|i| {
+                let r = v.push(i);
+                (r, r as *const usize)
+            })
+            .collect();
+        eprintln!("{refs:?}");
+        let first = &v[0];
+        let first = first as *const usize;
+        assert_eq!(first, refs[0].1);
     }
 }
